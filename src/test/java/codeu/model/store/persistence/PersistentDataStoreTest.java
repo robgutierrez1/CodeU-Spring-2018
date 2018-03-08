de00c54e7b22cb3d206ef4e2eb8c21fc8bc5c16b
hyang344 <hyang344@wisc.edu>
2018-03-08 16:05:48 -0600

    P1 part2 done
    Add password to datastore and update tests to check that the password is correctly loaded and stored from Datastore. Passed tests and compiles.

--- src/test/java/codeu/model/store/persistence/PersistentDataStoreTest.java
+++ src/test/java/codeu/model/store/persistence/PersistentDataStoreTest.java
@@ -59,11 +59,13 @@ public class PersistentDataStoreTest {
     User resultUserOne = resultUsers.get(0);
     Assert.assertEquals(idOne, resultUserOne.getId());
     Assert.assertEquals(nameOne, resultUserOne.getName());
+    Assert.assertEquals("password", resultUserOne.getPassword());
     Assert.assertEquals(creationOne, resultUserOne.getCreationTime());
 
     User resultUserTwo = resultUsers.get(1);
     Assert.assertEquals(idTwo, resultUserTwo.getId());
     Assert.assertEquals(nameTwo, resultUserTwo.getName());
+    Assert.assertEquals("password", resultUserTwo.getPassword());
     Assert.assertEquals(creationTwo, resultUserTwo.getCreationTime());
   }
 
