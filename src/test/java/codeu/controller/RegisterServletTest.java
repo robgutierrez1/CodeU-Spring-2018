c8c5ee8978d98189c33168bfaa84c55873db1dac
Robert Gutierrez <rgutierrez27@utexas.edu>
2018-03-17 15:16:30 -0500

    Tests to see if username is taken, new, and valid

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import codeu.model.store.basic.UserStore;
+import codeu.model.data.User;
+import javax.servlet.http.HttpSession;
+import org.junit.Assert;
+import org.mockito.ArgumentCaptor;
 
 public class RegisterServletTest {
 
@@ -32,4 +37,59 @@ public class RegisterServletTest {
 
    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
  }
+
+// Test a bad username
+@Test
+public void testDoPost_BadUsername() throws IOException, ServletException {
+  Mockito.when(mockRequest.getParameter("username"))
+  .thenReturn("1234_H3ll0!");
+
+  registerServlet.doPost(mockRequest, mockResponse);
+
+  Mockito.verify(mockRequest)
+      .setAttribute("error", "Please enter only letters, numbers, and spaces.");
+  Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+}
+
+// Test a new user
+@Test
+public void testDoPost_NewUser() throws IOException, ServletException {
+  Mockito.when(mockRequest.getParameter("username"))
+  .thenReturn("test username");
+
+  UserStore mockUserStore = Mockito.mock(UserStore.class);
+  Mockito.when(mockUserStore.isUserRegistered("test username"))
+  .thenReturn(false);
+  registerServlet.setUserStore(mockUserStore);
+
+  HttpSession mockSession = Mockito.mock(HttpSession.class);
+  Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
+  registerServlet.doPost(mockRequest, mockResponse);
+
+  ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+
+  Mockito.verify(mockUserStore).addUser(userArgumentCaptor.capture());
+  Assert.assertEquals(userArgumentCaptor.getValue().getName(), "test username");
+}
+
+// Test if username is already taken
+@Test
+public void testDoPost_ExistingUser() throws IOException, ServletException {
+  Mockito.when(mockRequest.getParameter("username"))
+  .thenReturn("test username");
+
+  UserStore mockUserStore = Mockito.mock(UserStore.class);
+  Mockito.when(mockUserStore.isUserRegistered("test username"))
+  .thenReturn(true);
+  registerServlet.setUserStore(mockUserStore);
+
+  HttpSession mockSession = Mockito.mock(HttpSession.class);
+  Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
+  registerServlet.doPost(mockRequest, mockResponse);
+
+  Mockito.verify(mockUserStore, Mockito.never())
+  .addUser(Mockito.any(User.class));
+  }
 }
