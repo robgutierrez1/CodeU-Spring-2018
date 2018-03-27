08c6c3717ba0e555ce9cddea6d034007a88e480e
Zené <zene514@gmail.com>
2018-03-26 19:39:59 -0500

    project 1, part 9 done

--- src/main/java/codeu/model/data/User.java
+++ src/main/java/codeu/model/data/User.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class User {
   private final UUID id;
   private final String name;
-  private final String password;
+  private final String hashedPassword;
   private final Instant creation;
 
   /**
@@ -35,7 +35,7 @@ public class User {
   public User(UUID id, String name, String password, Instant creation) {
     this.id = id;
     this.name = name;
-    this.password = password;
+    this.hashedPassword = password;
     this.creation = creation;
   }
 
@@ -53,8 +53,9 @@ public class User {
    * Returns the password of this User.
    */
   public String getPassword() {
-    return password;
+    return hashedPassword;
   }
+
 
   /** Returns the creation time of this User. */
   public Instant getCreationTime() {
