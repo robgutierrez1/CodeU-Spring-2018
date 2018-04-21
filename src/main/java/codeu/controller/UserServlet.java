336312bf8881ca10cdff3b3f73c5aa74123d68f2
hyang344 <hyang344@wisc.edu>
2018-04-21 13:50:50 -0500

    Reformat UserServlet
    Hopefully nothing bad happens this time.

--- src/main/java/codeu/controller/UserServlet.java
+++ src/main/java/codeu/controller/UserServlet.java
@@ -35,171 +35,172 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet class responsible for the conversations page. */
 public class UserServlet extends HttpServlet {
 
-  /** Store class that gives access to Users. */
-  private UserStore userStore;
-  
-  /** Store class that gives access to Conversations. */
-  private ConversationStore conversationStore;
-  
-  /** Store class that gives access to Messages. */
-  private MessageStore messageStore;
-  
-  /** Checks whether the user is editing about me*/
-  private Boolean editAboutMe = false;
-  
-  /** List of User Messages related to this user*/
-  private List<Message> userMessages;
+	/** Store class that gives access to Users. */
+	private UserStore userStore;
 
-  /**
-   * Set up state for handling user-related and conversation-related requests. This method 
-   * is only called when running in a server, not when running in a test.
-   */
-  @Override
-  public void init() throws ServletException {
-    super.init();
-    setUserStore(UserStore.getInstance());
-    setConversationStore(ConversationStore.getInstance());
-    setMessageStore(MessageStore.getInstance());
-  }
+	/** Store class that gives access to Conversations. */
+	private ConversationStore conversationStore;
 
-  /**
-   * Sets the UserStore used by this servlet. This function provides a common setup method for use
-   * by the test framework or the servlet's init() function.
-   */
-  void setUserStore(UserStore userStore) {
-    this.userStore = userStore;
-  }
-  
-  /**
-   * Sets the ConversationStore used by this servlet. This function provides a common setup method for use
-   * by the test framework or the servlet's init() function.
-   */
-  void setConversationStore(ConversationStore conversationStore) {
-    this.conversationStore = conversationStore;
-  }
-  
-  /**
-   * Sets the MessageStore used by this servlet. This function provides a common setup method for
-   * use by the test framework or the servlet's init() function.
-   */
-  void setMessageStore(MessageStore messageStore) {
-    this.messageStore = messageStore;
-  }
-  
-  /**
-   * Gets the list conversation of a particular user
-   */
-   List getUserConversations(User user) {
-     UUID userId = user.getId();
-     List<Conversation> allConversations = conversationStore.getAllConversations();
-     List<Conversation> userConversations = new ArrayList<Conversation>();   
-     for (Conversation conversation: allConversations) {
-       UUID conversationOwnerId = conversation.getOwnerId();
-       if (conversationOwnerId.equals(userId)) {
-         userConversations.add(conversation);
-       }
-     }
-     return userConversations;
-   }
-    
-  /**
-   * This function fires when a user navigates to the user page. It checks whether the username
-   * exist in the database and forwards to conversations.jsp for rendering the list.
-   */
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
-	  
-	String requestUrl = request.getRequestURI();
-	String userTitle = requestUrl.substring("/user/".length());
-	  
-	User user = userStore.getUser(userTitle);
-    if (user == null) {
-      // couldn't find user, redirect to home page (for now)
-      System.out.println("Name was null: " + userTitle);
-      response.sendRedirect("/");
-      return;
-    }
+	/** Store class that gives access to Messages. */
+	private MessageStore messageStore;
 
-    String viewerName = (String) request.getSession().getAttribute("user");
-    User viewer = userStore.getUser(viewerName);
-    String aboutMe = user.getAboutMe();
-	request.setAttribute("user", user);
-	request.setAttribute("viewer", viewer);
-	request.setAttribute("editAboutMe", editAboutMe);
-    request.setAttribute("aboutMe", aboutMe);
-	
-	List<Conversation> userConversations = getUserConversations(user);
-	userMessages = new ArrayList<Message>();
-	for (Conversation conversation: userConversations) {
-	  List<Message> messages = messageStore.getMessagesInConversation(conversation.getId());
-	  userMessages.addAll(messages);
+	/** Checks whether the user is editing about me */
+	private Boolean editAboutMe = false;
+
+	/** List of User Messages related to this user */
+	private List<Message> userMessages;
+
+	/**
+	 * Set up state for handling user-related and conversation-related requests.
+	 * This method is only called when running in a server, not when running in a
+	 * test.
+	 */
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		setUserStore(UserStore.getInstance());
+		setConversationStore(ConversationStore.getInstance());
+		setMessageStore(MessageStore.getInstance());
 	}
-	
-	// added to sort userMessages
-	Collections.sort(userMessages, new Comparator<Message>() {
-		@Override
-		public int compare(Message o1, Message o2) {
-			return o2.getCreationTime().compareTo(o1.getCreationTime());
+
+	/**
+	 * Sets the UserStore used by this servlet. This function provides a common
+	 * setup method for use by the test framework or the servlet's init() function.
+	 */
+	void setUserStore(UserStore userStore) {
+		this.userStore = userStore;
+	}
+
+	/**
+	 * Sets the ConversationStore used by this servlet. This function provides a
+	 * common setup method for use by the test framework or the servlet's init()
+	 * function.
+	 */
+	void setConversationStore(ConversationStore conversationStore) {
+		this.conversationStore = conversationStore;
+	}
+
+	/**
+	 * Sets the MessageStore used by this servlet. This function provides a common
+	 * setup method for use by the test framework or the servlet's init() function.
+	 */
+	void setMessageStore(MessageStore messageStore) {
+		this.messageStore = messageStore;
+	}
+
+	/**
+	 * Gets the list conversation of a particular user
+	 */
+	List getUserConversations(User user) {
+		UUID userId = user.getId();
+		List<Conversation> allConversations = conversationStore.getAllConversations();
+		List<Conversation> userConversations = new ArrayList<Conversation>();
+		for (Conversation conversation : allConversations) {
+			UUID conversationOwnerId = conversation.getOwnerId();
+			if (conversationOwnerId.equals(userId)) {
+				userConversations.add(conversation);
+			}
 		}
-	});
-	
-	List<Message> topTenList = new ArrayList<Message>();
-	for (int i = 0; i < userMessages.size() && i < 10; i++) {
-		topTenList.add(userMessages.get(i));
+		return userConversations;
 	}
-	
-	request.setAttribute("messages", topTenList);
-	
-    request.getRequestDispatcher("/WEB-INF/view/user.jsp").forward(request, response);
-  }
 
-  /**
-   * This function fires when a user submits a change in the about me section. It gets the
-   * logged-in username from the session and the new conversation title from the submitted form
-   * data. It uses this to create a new Conversation object that it adds to the model.
-   */
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException{
-	  
-	String requestUrl = request.getRequestURI();
-	String userTitle = requestUrl.substring("/user/".length());
-          
-    User user = userStore.getUser(userTitle);
-    if (user == null) {
-      // couldn't find user, redirect to home page (for now)
-      System.out.println("Name was null: " + userTitle);
-      response.sendRedirect("/");
-      return;
-    }
-	
-	String buttonVal = request.getParameter("buttonVal");
-    String enteredAboutMe = request.getParameter("enteredAboutMe");
-	
-	if (buttonVal.equals("edit")) {
-		editAboutMe = true;
-		System.out.println("Edit!");
-	} else if (buttonVal.equals("cancel")) {
-		editAboutMe = false;
-		System.out.println("Cancel!");
-	} else if (buttonVal.equals("submit")){
-		editAboutMe = false; // finish editing
-        user.setAboutMe(enteredAboutMe);
-        userStore.updateAboutMe(user, enteredAboutMe);
-		System.out.println("Submit!");
-	} else if(buttonVal.contains("hide")) {
-		int orderOfMessage = Integer.parseInt(buttonVal.substring(4));
-		userMessages.get(orderOfMessage).setOpenToPublic(false);
-	} else if(buttonVal.contains("show")) {
-		int orderOfMessage = Integer.parseInt(buttonVal.substring(4));
-		userMessages.get(orderOfMessage).setOpenToPublic(true);
-	} else {
-		// default case: shouldn't reach here
-		System.out.println("Something went wrong...");
+	/**
+	 * This function fires when a user navigates to the user page. It checks whether
+	 * the username exist in the database and forwards to conversations.jsp for
+	 * rendering the list.
+	 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String requestUrl = request.getRequestURI();
+		String userTitle = requestUrl.substring("/user/".length());
+
+		User user = userStore.getUser(userTitle);
+		if (user == null) {
+			// couldn't find user, redirect to home page (for now)
+			System.out.println("Name was null: " + userTitle);
+			response.sendRedirect("/");
+			return;
+		}
+
+		String viewerName = (String) request.getSession().getAttribute("user");
+		User viewer = userStore.getUser(viewerName);
+		String aboutMe = user.getAboutMe();
+		request.setAttribute("user", user);
+		request.setAttribute("viewer", viewer);
+		request.setAttribute("editAboutMe", editAboutMe);
+		request.setAttribute("aboutMe", aboutMe);
+
+		List<Conversation> userConversations = getUserConversations(user);
+		userMessages = new ArrayList<Message>();
+		for (Conversation conversation : userConversations) {
+			List<Message> messages = messageStore.getMessagesInConversation(conversation.getId());
+			userMessages.addAll(messages);
+		}
+
+		// added to sort userMessages
+		Collections.sort(userMessages, new Comparator<Message>() {
+			@Override
+			public int compare(Message o1, Message o2) {
+				return o2.getCreationTime().compareTo(o1.getCreationTime());
+			}
+		});
+
+		List<Message> topTenList = new ArrayList<Message>();
+		for (int i = 0; i < userMessages.size() && i < 10; i++) {
+			topTenList.add(userMessages.get(i));
+		}
+
+		request.setAttribute("messages", topTenList);
+
+		request.getRequestDispatcher("/WEB-INF/view/user.jsp").forward(request, response);
 	}
-	  
-	response.sendRedirect("/user/" + userTitle);
-  }
+
+	/**
+	 * This function fires when a user submits a change in the about me section. It
+	 * gets the logged-in username from the session and the new conversation title
+	 * from the submitted form data. It uses this to create a new Conversation
+	 * object that it adds to the model.
+	 */
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String requestUrl = request.getRequestURI();
+		String userTitle = requestUrl.substring("/user/".length());
+
+		User user = userStore.getUser(userTitle);
+		if (user == null) {
+			// couldn't find user, redirect to home page (for now)
+			System.out.println("Name was null: " + userTitle);
+			response.sendRedirect("/");
+			return;
+		}
+
+		String buttonVal = request.getParameter("buttonVal");
+		String enteredAboutMe = request.getParameter("enteredAboutMe");
+
+		if (buttonVal.equals("edit")) {
+			editAboutMe = true;
+			System.out.println("Edit!");
+		} else if (buttonVal.equals("cancel")) {
+			editAboutMe = false;
+			System.out.println("Cancel!");
+		} else if (buttonVal.equals("submit")) {
+			editAboutMe = false; // finish editing
+			user.setAboutMe(enteredAboutMe);
+			userStore.updateAboutMe(user, enteredAboutMe);
+			System.out.println("Submit!");
+		} else if (buttonVal.contains("hide")) {
+			int orderOfMessage = Integer.parseInt(buttonVal.substring(4));
+			userMessages.get(orderOfMessage).setOpenToPublic(false);
+		} else if (buttonVal.contains("show")) {
+			int orderOfMessage = Integer.parseInt(buttonVal.substring(4));
+			userMessages.get(orderOfMessage).setOpenToPublic(true);
+		} else {
+			// default case: shouldn't reach here
+			System.out.println("Something went wrong...");
+		}
+
+		response.sendRedirect("/user/" + userTitle);
+	}
 }
-
